dfa5b9a363262810517a0b0271a425f4326ab996
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-11-03 17:52:40 +0800

    some fix and companyDetails page

---
+++ src/assets/svg/arrowUp.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+export const ArrowUp = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+    >
+      <path
+        d="M9.33366 13.3333L16.0003 20L22.667 13.3333"
+        stroke="#DD005C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <rect
+        x="31.5"
+        y="31.5"
+        width="31"
+        height="31"
+        rx="15.5"
+        transform="rotate(-180 31.5 31.5)"
+        stroke="#9C9B9B"
+      />
+    </svg>
+  );
+};
