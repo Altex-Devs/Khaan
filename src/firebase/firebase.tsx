ee997cb1b4b2f3a06ec7f4e3ade5e40cf967af1b
Elaman <yelamanjambal@gmail.com>
2024-03-27 10:27:43 +0800

    Google analytic changes

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyD7WWS5KZpk5uh0wKgMLo2Qsnc71LFTc-I",
   authDomain: "khaan-e3b0c.firebaseapp.com",
@@ -11,7 +11,8 @@ const firebaseConfig = {
   appId: "1:847608610736:web:4091ef1a747065bbc22075",
   measurementId: "G-L8BBC6FDPL",
 };
-
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
+
