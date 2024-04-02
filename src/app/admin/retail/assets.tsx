f109efe28c7ceea9475b070eaf2f5efa1170b5fc
Elaman <yelamanjambal@gmail.com>
2024-04-02 10:38:05 +0800

    Khaan dashboard
    Admin dashboard page added

---
+++ src/app/admin/retail/assets.tsx
@@ -0,0 +1,12 @@
+export default function Assets() {
+    return (
+      <ul className="list">
+        <li>Making WebViews work for the Web</li>
+        <li>Remote Meeting Agenda in Development</li>
+        <li>W3C Accessibility Maturity Model</li>
+        <li>Stepping forward on WAI management</li>
+      </ul>
+    );
+  }
+  
+  
