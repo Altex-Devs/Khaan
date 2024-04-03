d58513dd4f00075c98d03e9024ac76f3890fa4ae
Elaman <yelamanjambal@gmail.com>
2024-04-03 16:36:53 +0800

    Tur comment bolgov

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,29 +1,29 @@
-"use client";
+// "use client";
 
-import type { ReactNode } from "react";
-import AdminNav from './components/navbar/adminNav';
-import "./style.css";
-import { Tab, TabList, TabPanel, Tabs } from "./tabs";
-const Admin = (props: { tabs: ReactNode }) => {
-    return (
-        <div className="text-white flex h-screen">
-            <AdminNav />
-            <main className="flex-grow p-10 bg-[#e9ebf0] ml-40 fontSomething">
-                <h1 className="text-3xl font-semibold mb-6 text-[#66377B]">Иргэд</h1>
-                <div className="wrapper">
-                    <Tabs>
-                        <TabList>
-                            <Tab href="/admin/retail/health">Эрүүл мэнд, гэнэтийн осол</Tab>
-                            <Tab href="/admin/retail/assets">Хөрөнгийн даатгал</Tab>
-                            <Tab href="/admin/retail/response">Хариуцлагын даатгал</Tab>
-                            <Tab href="/admin/retail/finance">Санхүүгийн даатгал</Tab>
-                        </TabList>
-                        <TabPanel>{props.tabs}</TabPanel>
-                    </Tabs>
-                </div>
-            </main>
-        </div>
-    );
-}
+// import type { ReactNode } from "react";
+// import AdminNav from './components/navbar/adminNav';
+// import "./style.css";
+// import { Tab, TabList, TabPanel, Tabs } from "./tabs";
+// const Admin = (props: { tabs: ReactNode }) => {
+//     return (
+//         <div className="text-white flex h-screen">
+//             <AdminNav />
+//             <main className="flex-grow p-10 bg-[#e9ebf0] ml-40 fontSomething">
+//                 <h1 className="text-3xl font-semibold mb-6 text-[#66377B]">Иргэд</h1>
+//                 <div className="wrapper">
+//                     <Tabs>
+//                         <TabList>
+//                             <Tab href="/admin/retail/health">Эрүүл мэнд, гэнэтийн осол</Tab>
+//                             <Tab href="/admin/retail/assets">Хөрөнгийн даатгал</Tab>
+//                             <Tab href="/admin/retail/response">Хариуцлагын даатгал</Tab>
+//                             <Tab href="/admin/retail/finance">Санхүүгийн даатгал</Tab>
+//                         </TabList>
+//                         <TabPanel>{props.tabs}</TabPanel>
+//                     </Tabs>
+//                 </div>
+//             </main>
+//         </div>
+//     );
+// }
 
 // export default Admin;
