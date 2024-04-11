ac54090ba366ab69e9efcd6a74d1868533e50f04
Elaman <yelamanjambal@gmail.com>
2024-04-11 12:27:26 +0700

    Warning taiwlind ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()],
-};
+  plugins: [],
+}
+
